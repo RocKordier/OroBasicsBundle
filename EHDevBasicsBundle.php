2c02b30fe71190e92081cdd3d669aa11dc584d82
Eric Hertwig <eric@ehdev.de>
2017-01-08 16:37:56 +0100

    AutoLoad Bundle with Oro

---
+++ EHDevBasicsBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace EHDev\Bundle\BasicsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EHDevBasicsBundle
+ *
+ * @package EHDev\Bundle\BasicsBundle
+ */
+class EHDevBasicsBundle extends Bundle
+{
+}
