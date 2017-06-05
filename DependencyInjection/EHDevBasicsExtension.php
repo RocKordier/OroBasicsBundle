b7775430f5b7398b035d6385e9d02a1f6321fc33
Eric Hertwig <eric@ehdev.de>
2017-06-05 21:18:37 +0200

    FA-25 Remove all SimpleContact and Address References I'm still thinking that in some cases we need a lightweight version of the OroContact, but this isn't the right place for it.

--- DependencyInjection/EHDevBasicsExtension.php
+++ DependencyInjection/EHDevBasicsExtension.php
@@ -17,7 +17,5 @@ class EHDevBasicsExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('form.yml');
     }
 }
