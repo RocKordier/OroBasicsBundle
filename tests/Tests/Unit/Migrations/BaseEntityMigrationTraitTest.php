c6bb0f7eb01e3e441f44f832c5e31452f7e6f3f2
David Korm <david.korm@gmx.net>
2018-01-19 00:12:07 +0100

    Refactor BaseEntityMigrationTraitTest

--- tests/Tests/Unit/Migrations/BaseEntityMigrationTraitTest.php
+++ tests/Tests/Unit/Migrations/BaseEntityMigrationTraitTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace EHDev\BasicsBundle\Tests\Unit\Migrations;
 
+use Doctrine\DBAL\Types\DateTimeType;
+use Doctrine\DBAL\Types\IntegerType;
 use EHDev\BasicsBundle\Tests\PHPUnitUtil;
 use PHPUnit\Framework\TestCase;
 use EHDev\BasicsBundle\Tests\Fixture\BaseEntityMigrationTraitTestClass;
@@ -11,6 +13,9 @@ use Doctrine\DBAL\Schema\Table;
  */
 class BaseEntityMigrationTraitTest extends TestCase
 {
+    /**
+     * @covers ::migrateBaseEntity
+     */
     public function testMigrationTrait()
     {
         try {
@@ -20,6 +25,13 @@ class BaseEntityMigrationTraitTest extends TestCase
         }
         $migrateBaseEntityMethod = PHPUnitUtil::callMethod(new BaseEntityMigrationTraitTestClass(), 'migrateBaseEntity', [$table]);
 
+        $this->assertTrue($table->hasColumn('id'));
+        $this->assertTrue($table->hasColumn('created_at'));
+        $this->assertTrue($table->hasColumn('updated_at'));
+        $this->assertInstanceOf(IntegerType::class, $table->getColumn('id')->getType());
+        $this->assertInstanceOf(DateTimeType::class, $table->getColumn('created_at')->getType());
+        $this->assertInstanceOf(DateTimeType::class, $table->getColumn('updated_at')->getType());
+        $this->assertTrue($table->hasPrimaryKey());
         $this->assertNull($migrateBaseEntityMethod);
     }
 }
