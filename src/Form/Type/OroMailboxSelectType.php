baee9dde1c5a64d30c171541d17b69096afd1410
Eric Hertwig <eric@ehdev.de>
2018-02-26 05:01:13 +0100

    Add OroMailbox select type

---
+++ src/Form/Type/OroMailboxSelectType.php
@@ -0,0 +1,34 @@
+<?php
+namespace EHDev\BasicsBundle\Form\Type;
+
+use Oro\Bundle\EmailBundle\Entity\Mailbox;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OroMailboxSelectType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'class' => Mailbox::class,
+            ]
+        );
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'ehdev_oro_mailbox_select';
+    }
+
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+}
