0d9b713c3a844c0a52ed90aae6af016e3f0e33df
RocKordier <eric@ehdev.de>
2022-07-09 22:21:02 +0200

    Use CreatedUpdated Trait with Gedmo in favour of Lifecycle Trait

--- src/Entity/Base.php
+++ src/Entity/Base.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace EHDev\BasicsBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use EHDev\BasicsBundle\Entity\Traits\LifecycleTrait;
+use EHDev\BasicsBundle\Entity\Traits\CreatedUpdatedTrait;
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
 
 #[ORM\HasLifecycleCallbacks]
 #[ORM\MappedSuperclass]
 class Base
 {
-    use LifecycleTrait;
+    use CreatedUpdatedTrait;
 
     #[ORM\Column('id', type: 'integer')]
     #[ORM\Id]
